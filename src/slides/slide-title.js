cb1258780a7e6e0d5c651084df565129d83f19a6
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2021-01-22 00:39:02 +0000

    working landing page

---
+++ src/slides/slide-title.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './slides.css';
+
+function Title() {
+  return (
+    <div className="container title">
+      <div className="slide light">
+        <div className="slide">
+          <h1>ProGEL</h1>
+          <div className="title-line"></div>
+          <h2>Bringing the power of mRNA technologies to <nobr>anyone, anywhere</nobr></h2>
+          <a target="_top" className="contact-button" href="https://imperiallondon.sharepoint.com/sites/VCCSoftNano-MM/Shared%20Documents/General/Research/ProGel%20Draft%20Design.pptx?web=0?download.aspx">
+            <h2>Download Pitch Deck</h2>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Title;
